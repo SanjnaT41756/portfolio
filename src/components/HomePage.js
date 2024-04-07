a17fd62b038fcdf2edd7aef89978384dca5aabc6
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-04-06 21:41:51 -0700

    initial font and color style setup

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/theme.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <header className="big-header">
+                <h>I'm Sanjna</h>
+            </header>
+            <main>
+                {/* Add your content here */}
+            </main>
+        </div>
+    );
+};
+
+export default HomePage;
