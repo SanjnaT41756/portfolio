6ca955c45120c4b281bd84486ecb08d729dd0e96
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-04-07 21:28:14 -0700

    add header and styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,9 +4,10 @@ import '../styles/theme.css';
 const HomePage = () => {
     return (
         <div>
-            <header className="big-header">
-                <h>I'm Sanjna</h>
-            </header>
+            <p className="paragraph" style={{ marginBottom: '5px', fontSize: '0.1em', fontWeight: 'lighter'}}> <i>Welcome to the Sunny Side...</i></p>
+
+            <h className="big-header">I'm Sanjna</h>
+            <p className="paragraph" style={{ marginTop: '10px'}}>I'm a full-stack web developer and a student at the University of Washington. I'm passionate about creating beautiful, user-friendly websites and applications. I'm also a huge fan of coffee, dogs, and the color yellow.</p>
             <main>
                 {/* Add your content here */}
             </main>
