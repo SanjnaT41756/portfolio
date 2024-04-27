86a076126cddd664e53a214ff436b14d3d2c02b3
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-04-26 18:32:52 -0700

    add subheading

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Menu from './Menu';
 import '../styles/theme.css';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
     return (
         <div>
+            <div className="container">
+
+            <div className="header">
+                <p className="paragraph" style={{ marginBottom: '5px', fontSize: '28px', fontWeight: 'lighter'}}> <i>Welcome to the Sunny Side...</i></p>
+                <h2 className="big-header">I'm Sanjna</h2>
+                <p className="paragraph" style={{ marginTop: '10px'}}>I'm a full-stack web developer and a student at the University of Washington. I'm passionate about creating beautiful, user-friendly websites and applications. I'm also a huge fan of coffee, dogs, and the color yellow.</p>
+            </div>
             <Menu />
-            <p className="paragraph" style={{ marginBottom: '5px', fontSize: '28px', fontWeight: 'lighter'}}> <i>Welcome to the Sunny Side...</i></p>
-            <h className="big-header">I'm Sanjna</h>
-            <p className="paragraph" style={{ marginTop: '10px'}}>I'm a full-stack web developer and a student at the University of Washington. I'm passionate about creating beautiful, user-friendly websites and applications. I'm also a huge fan of coffee, dogs, and the color yellow.</p>
+            </div>
             <main>
-                {/* Add your content here */}
+                <div className="main-content">
+                    <div className="subheading">
+                        <h2 className="small-header">Projects</h2>
+                    </div>
+                </div>
             </main>
         </div>
     );
