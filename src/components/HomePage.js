29cd3a8f1d7fb34891d52a2e6ec09b27e139a71a
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-08-25 13:16:48 -0700

    Update Project Card sizing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,15 +13,23 @@ const HomePage = () => {
                 <div className="header">
                     <p className="paragraph" style={{ marginBottom: '5px', fontSize: '28px', fontWeight: 'lighter'}}> <i>Welcome to the Sunny Side...</i></p>
                     <h2 className="big-header">I'm Sanjna</h2>
-                    <p className="paragraph" style={{ marginTop: '10px'}}>I'm a full-stack web developer and a student at the University of Washington. I'm passionate about creating beautiful, user-friendly websites and applications. I'm also a huge fan of coffee, dogs, and the color yellow.</p>
+                    <p className="paragraph" style={{ marginTop: '10px'}}>
+                        < p className='about'>
+                        I'm a full-stack web developer and a student at the University of Washington. I'm passionate about creating beautiful, user-friendly websites and applications. I'm also a huge fan of coffee, dogs, and the color yellow.
+                        </p>
+                    </p>
                 </div>
                 <Menu />
             </div>
             <main>
-                <div className="main-content" id = "Projects">
+
                     <div className="subheading">
-                        <h2 className="small-header">Projects</h2>
+                        <h2 className="small-header">Projects and Experience</h2>
                     </div>
+                <div className="main-content" id = "Projects">
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
                     <ProjectCard />
                     <ProjectCard />
                     <ProjectCard />
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,10 +5,14 @@ import '../styles/theme.css';
 const ProjectCard = () => {
     return (
         <div className='card-container'>
-            <img src='https://via.placeholder.com/200' alt='Project' className='card-image' />
+            <img src='https://via.placeholder.com/150x70' alt='Placeholder' className='placeholder-image' />
             <div>
                 <h3 className='card-title'>Project Title</h3>
-                <p className='paragraph'>This is a brief description of the project. It should be a few sentences long.</p>
+                <p className='paragraph'>
+                    <p className='content'>
+                    This is a brief description of the project. It should be a few sentences long.
+                    </p>
+                </p>
             </div>
         </div>
     );
