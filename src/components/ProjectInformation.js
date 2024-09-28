ce113ae8ff42802bd4ece15cc5fbe716165d3812
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-09-28 15:23:38 -0700

    Add project repository links

--- src/components/ProjectInformation.js
+++ src/components/ProjectInformation.js
@@ -4,6 +4,7 @@ import projectsData from '../projects.json';
 import BackButton from './BackButton';
 import '../styles/ProjectInformation.css';
 import '../styles/theme.css';
+import Github from '../styles/images/Github.svg';
 const ProjectInformation = () => {
   const { projectId } = useParams();
   const project = projectsData[projectId];
@@ -19,18 +20,22 @@ const ProjectInformation = () => {
       <h3 className='small-header' style={{ paddingRight: '0' }}>{project.title}</h3>
       <div className='project-content'>
         <div className='section'>
-              <p className='paragraph'>{project.info}</p>
-              <img src='https://via.placeholder.com/300x300' alt='Placeholder' className='placeholder-image' />
+          <p className='paragraph'>{project.info}</p>
+          <img src='https://via.placeholder.com/150x150' alt='Placeholder' className='placeholder-image' />
         </div>
 
         <div className='section'>
-          <img src='https://via.placeholder.com/300x300' alt='Placeholder' className='placeholder-image' />
+          <img src='https://via.placeholder.com/150x150' alt='Placeholder' className='placeholder-image' />
           <p className='paragraph'>{project.timeContributions}</p>
         </div>
 
-        <div>
-          <p className='paragraph'>Check out the project repository!</p>
+        <a href={project.link} target="_blank" rel="noreferrer" style={{textDecoration: "none"}}>
+        <div className='repoButton' >
+            <p className='paragraph' style={{color:"white"}}>Check out the project repository!</p>
+            <img src={Github} alt="Project Github Repository" style={{width:"30px"}}/>
+
         </div>
+        </a>
 
       </div>
     </div>
